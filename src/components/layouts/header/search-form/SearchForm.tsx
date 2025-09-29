0a4ff7308d7558e60ff91b229848310fa749ddc2
Ali <alitaghipour95@yahoo.com>
2025-09-29 14:50:01 +0330

    feat: add search-form component

---
+++ src/components/layouts/header/search-form/SearchForm.tsx
@@ -0,0 +1,13 @@
+import { IconBox } from "@/components/common";
+
+export function SearchForm() {
+    // TODO should implement this search form
+  return (
+    <form name="search-form" action="#" method="post" className="flex items-center">
+        <input type="text" name="search_text" placeholder="Search for items" className="text-xsmall text-gray-400 border-gray-300 w-full py-[15px] focus:outline-none" />
+        <button type="submit">
+            <IconBox icon={"icon-search"}/>
+        </button>
+    </form>
+  )
+}
