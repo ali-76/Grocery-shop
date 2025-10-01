e308ace89abfb1e9e3eb1acccf4c7e335995d7cc
Ali <alitaghipour95@yahoo.com>
2025-10-01 11:39:55 +0330

    update : search-form component

--- src/components/layouts/header/search-form/SearchForm.tsx
+++ src/components/layouts/header/search-form/SearchForm.tsx
@@ -4,7 +4,7 @@ export function SearchForm() {
     // TODO should implement this search form
   return (
     <form name="search-form" action="#" method="post" className="flex items-center">
-        <input type="text" name="search_text" placeholder="Search for items" className="text-xsmall text-gray-400 border-gray-300 w-full py-[15px] focus:outline-none" />
+        <input type="text" name="search_text" placeholder="Search for items" className="text-xsmall text-gray-600 border-gray-300 w-full py-[15px] focus:outline-none" />
         <button type="submit">
             <IconBox icon={"icon-search"}/>
         </button>
