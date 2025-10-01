d881f4837a6653cf3c3a911e2e166120cc954234
Ali <alitaghipour95@yahoo.com>
2025-10-01 14:52:07 +0330

    feat: add browsCategory mock

---
+++ src/mock/browsCategory.ts
@@ -0,0 +1,69 @@
+interface Props {
+    icon : string ;
+    link : string ;
+    title : string ;
+    path : number ;
+}
+
+export const browsCategoryMock : Props[] = [
+    {
+        icon : "groceries" ,
+        link : "#" ,
+        title : "Milk and Dairies"  ,
+        path : 7 
+    },
+    {
+        icon : "dress" ,
+        link : "#" ,
+        title : "Clothing & beauty"  ,
+        path : 4 
+    },
+    {
+        icon : "pet-food" ,
+        link : "#" ,
+        title : "Pet Foods & Toy"  ,
+        path : 8 
+    },
+    {
+        icon : "dairy-products" ,
+        link : "#" ,
+        title : "Baking material" ,
+        path : 5 
+    },
+    {
+        icon : "fruit" ,
+        link : "#" ,
+        title : "Fresh Fruit" ,
+        path : 3 
+    },
+    {
+        icon : "drinks" ,
+        link : "#" ,
+        title : "Wines & Drinks" ,
+        path : 5 
+    },
+    {
+        icon : "crab" ,
+        link : "#" ,
+        title : "Fresh Seafood" ,
+        path : 9 
+    },
+    {
+        icon : "sweet" ,
+        link : "#" ,
+        title : "Fast food" ,
+        path : 7 
+    },
+    {
+        icon : "grocery" ,
+        link : "#" ,
+        title : "Vegetables" ,
+        path : 7 
+    },
+    {
+        icon : "milk-products" ,
+        link : "#" ,
+        title : "Bread and Juice" ,
+        path : 5 
+    },
+]
