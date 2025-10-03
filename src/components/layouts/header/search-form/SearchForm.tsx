44c25410f5d34696e4b4a4c2af0462c1e57abeed
Ali <alitaghipour95@yahoo.com>
2025-10-03 14:05:30 +0330

    update: search-form component

--- src/components/layouts/header/search-form/SearchForm.tsx
+++ src/components/layouts/header/search-form/SearchForm.tsx
@@ -1,12 +1,16 @@
 import { IconBox } from "@/components/common";
 
-export function SearchForm() {
+interface Props{
+  inputClassName? : string;
+}
+
+export function SearchForm({inputClassName} : Props) {
     // TODO should implement this search form
   return (
     <form name="search-form" action="#" method="post" className="flex items-center">
-        <input type="text" name="search_text" placeholder="Search for items" className="text-xsmall text-gray-600 border-gray-300 w-full py-[15px] focus:outline-none" />
+        <input type="text" name="search_text" placeholder="Search for items" className={`text-xsmall text-gray-600 border-gray-300 w-full focus:outline-none ${{inputClassName}}`} />
         <button type="submit">
-            <IconBox icon={"icon-search"}/>
+            <IconBox icon={"icon-search"} iconClassname={"ml-1"}/>
         </button>
     </form>
   )
