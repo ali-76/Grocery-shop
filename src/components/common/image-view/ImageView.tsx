7351814e1a78206fd131346038737719bb64eff1
Ali <alitaghipour95@yahoo.com>
2025-10-04 19:02:52 +0330

    update: add fill property to component

--- src/components/common/image-view/ImageView.tsx
+++ src/components/common/image-view/ImageView.tsx
@@ -1,24 +1,36 @@
 import Image from "next/image";
 
 interface Props {
-  src : string;
-  alt : string;
-  width : number;
-  height : number;
-  classname? : string;
-  priority? : boolean
-
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+  className?: string;
+  priority?: boolean;
+  fill?: boolean;
 }
 
-export function ImageView({src , alt , width , height , classname = "" , priority = false} : Props) {
+export function ImageView({src , alt , width , height , className = "" , priority = false , fill = false}: Props) {
+  if (fill) {
+    return (
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        className={className}
+        priority={priority}
+      />
+    );
+  }
+
   return (
-    <Image 
+    <Image
       src={src}
       alt={alt}
       width={width}
       height={height}
-      className={classname}
+      className={className}
       priority={priority}
     />
-  )
+  );
 }
