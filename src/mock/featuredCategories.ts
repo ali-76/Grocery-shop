a908881e49159405e0afdef55eaf694a0ccc9317
Ali <alitaghipour95@yahoo.com>
2025-10-06 17:32:36 +0330

    feat: add featured-categories component

---
+++ src/mock/featuredCategories.ts
@@ -0,0 +1,81 @@
+
+interface Props {
+    title : string;
+    link : string;
+    img : string;
+    color : string;
+    items : number;
+}
+
+export const featuredCategoriesMock : Props[] = [
+    {
+        title : "Cake & milk",
+        link : '#',
+        img : "/assets/images/featured/cake-milch.png",
+        color : "#F2FCE4",
+        items : 14
+    },
+    {
+        title : "Peach",
+        link : '#',
+        img : "/assets/images/featured/peach.png",
+        color : "#FFFCEB",
+        items : 17
+    },
+    {
+        title : "Organic Kiwi",
+        link : '#',
+        img : "/assets/images/featured/organic-kiwi.png",
+        color : "#ECFFEC",
+        items : 21
+    },
+    {
+        title : "Red Apple",
+        link : '#',
+        img : "/assets/images/featured/red-apple.png",
+        color : "#FEEFEA",
+        items : 68
+    },
+    {
+        title : "Snack",
+        link : '#',
+        img : "/assets/images/featured/snack.png",
+        color : "#FFF3EB",
+        items : 34
+    },
+    {
+        title : "Black plum",
+        link : '#',
+        img : "/assets/images/featured/black-plum.png",
+        color : "#FFF3FF",
+        items : 25
+    },
+    {
+        title : "Vegetables",
+        link : '#',
+        img : "/assets/images/featured/vegetables.png",
+        color : "#F2FCE4",
+        items : 65
+    },
+    {
+        title : "Headphone",
+        link : '#',
+        img : "/assets/images/featured/headphone.png",
+        color : "#FFFCEB",
+        items : 54
+    },
+    {
+        title : "Cake & Milk",
+        link : '#',
+        img : "/assets/images/featured/snack.png",
+        color : "#F2FCE4",
+        items : 54
+    },
+    {
+        title : "Orange",
+        link : '#',
+        img : "/assets/images/featured/oranges.png",
+        color : "#FFF3FF",
+        items : 63
+    } 
+]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import { Banner, Section } from "@/components";
+import { Banner, FeaturedCategories, Section } from "@/components";
 
 export default function Home() {
   return (
     <main>
       <Section>
-        <Banner title={"Don’t miss amazing grocery deals"} subTitle={"Sign up for the daily newsletter"} img={"/assets/images/fresh-apples.png"} bgImg={"/assets/images/banner_bg.png"}/>
+        <Banner title={"Don’t miss amazing grocery deals"}
+        subTitle={"Sign up for the daily newsletter"}
+        img={"/assets/images/fresh-apples.png"}
+        bgImg={"/assets/images/banner_bg.png"}/>
+      </Section>
+
+      <Section className="mb-[75px]">
+        <div className="hidden sm:flex mb-[50px]">
+          <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
+        </div>
+        <FeaturedCategories />
       </Section>
     </main>
   );
