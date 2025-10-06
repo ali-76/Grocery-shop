8dc4524b0eedfd13662bf41c62af88e9862900bd
Ali <alitaghipour95@yahoo.com>
2025-10-06 16:17:02 +0330

    feat: add banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import { Banner, Section } from "@/components";
 
 export default function Home() {
   return (
-    <>this is main</>
+    <main>
+      <Section>
+        <Banner title={"Don’t miss amazing grocery deals"} subTitle={"Sign up for the daily newsletter"} img={"/assets/images/fresh-apples.png"} bgImg={"/assets/images/banner_bg.png"}/>
+      </Section>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,10 @@ const config: Config = {
         c: '14px 14px 36px 0px rgba(85, 85, 85, 0.1)',
         'c-lg': '14px 14px 36px 0px rgba(85, 85, 85, 0.22)'
       },
+      // backgroundImage: {
+      //   'hero-pattern': "url('../../images/banner_bg.png')",
+      //   'hero-img': "url('../../images/hero-img.png')"
+      // },
       fontFamily: {
         lato: ['var(--font-lato)']
       },
