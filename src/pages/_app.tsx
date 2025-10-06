a4c57661a53be2e272ea71f5e03c390ab389c50a
Ali <alitaghipour95@yahoo.com>
2025-10-06 11:39:18 +0330

    feat: add Quicksand & Lato fonts globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,33 @@ import { Layout } from "@/components";
 import "@/styles/globals.css";
 import "@/styles/icons.css";
 import type { AppProps } from "next/app";
+import { Lato, Quicksand } from 'next/font/google'
+
+const quicksand = Quicksand({
+  subsets: ['latin']
+});
+
+const lato = Lato({
+  weight: ['100' , '300' , '400'],
+  subsets: ['latin'],
+  variable: '--font-lato',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${quicksand.style.fontFamily}, sans-serif;
+          --font-lato: ${lato.style.fontFamily}, sans-serif;
+        }
+      `}</style>
+
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
